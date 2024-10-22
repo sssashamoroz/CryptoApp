01c6cde20a09d551a10ee17ac0baf78779724f3d
Sasha Moroz <aleksandr.shane@gmail.com>
2024-10-22 12:35:25 -0600

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Aleksandr Morozov on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoAppTests/CryptoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppTests.swift
+//  CryptoAppTests
+//
+//  Created by Aleksandr Morozov on 22/10/24.
+//
+
+import Testing
+@testable import CryptoApp
+
+struct CryptoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
